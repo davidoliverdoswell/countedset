94195fb98836ee2e239a7f349bd6b3894f0bd887
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-09-04 16:25:43 -0700

    updated data structure and cleaned up funcs

--- Generics/Generics/CountedSet.swift
+++ Generics/Generics/CountedSet.swift
@@ -16,7 +16,7 @@ struct CountedSet<Element> where Element: Hashable {
     
     // backing storage
     
-    private(set) var items: [Element] = []
+    private(set) var items: [Element:Int] = [:]
     
     // Hashable protocol conformance
     
@@ -25,31 +25,39 @@ struct CountedSet<Element> where Element: Hashable {
     // initialize class properties
     
     init(_ elements: [Element]) {
-        items = elements.reversed()
-        
         self.count = elements.count
     }
     
     // insert new element
     
-    mutating func insert(_ newElement: Element) {
-        items.insert(newElement, at: 0)
-        
+    mutating func insert(_ newElement: Element) -> Int {
+        items[newElement, default: 0] += 1
+        return items[newElement] ?? 0
+
     }
     
     // remove last element
     
-    mutating func remove(_ oldElement: Int) -> Element? {
-        return items.remove(at: oldElement)
+    mutating func remove(_ oldElement: Element) -> Int {
+        let item = items[oldElement, default: 0]
+        
+        if item > 1 {
+            items[oldElement] = item - 1
+            return item - 1
+        }
+        
+        if item == 1 {
+            items.removeValue(forKey: oldElement)
+            return 0
+        }
+        
+        return items.count
     }
     
     // support subscripting to look up current values
     // return 0 for any value that is not found.
     
     subscript(_ member: Element) -> Int {
-        if items.count <= member.hashValue {
-            return 0
-        }
-        return member.hashValue
+        return items[member] ?? 0
     }
 }
